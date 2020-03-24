f72d18f91a3b53a1820ad006e78eb6d3db6186b2
garciaoma <ogarciamg@outlook.com>
2020-03-23 19:33:45 -0600

    Adding eureka

---
+++ src/main/java/com/app/medical/register/MedicalRegisterApplication.java
@@ -0,0 +1,15 @@
+package com.app.medical.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MedicalRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/medical/register/MedicalRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.medical.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
